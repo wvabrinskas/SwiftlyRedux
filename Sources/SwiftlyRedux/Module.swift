7028da45e9aa02539ddb99b1c4eeccc1e3e6faa2
William Vabrinskas <williamvabrinskas@Williams-MacBook-Pro.local>
2021-10-11 21:01:25 -0400

    removed hashable gp

--- Sources/SwiftlyRedux/Module.swift
+++ Sources/SwiftlyRedux/Module.swift
@@ -4,10 +4,9 @@
 import Foundation
 import Combine
 
-public protocol Module: ObservableObject, Hashable {
+public protocol Module: ObservableObject {
   associatedtype ObjectType
   //Should be @Published in implementation, no way to do this yet in a protocol
-  var id: UUID { get set }
   var object: ObjectType? { get set }
   var objectPublisher: Published<ObjectType?>.Publisher { get }
 }
