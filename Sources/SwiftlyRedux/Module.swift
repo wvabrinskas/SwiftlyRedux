3f170c18cd50f4d5a9a721fc3db9818aaf50d07e
William Vabrinskas <williamvabrinskas@Williams-MacBook-Pro.local>
2021-10-11 21:06:34 -0400

    added id

--- Sources/SwiftlyRedux/Module.swift
+++ Sources/SwiftlyRedux/Module.swift
@@ -4,10 +4,16 @@
 import Foundation
 import Combine
 
-public protocol Module: ObservableObject {
+public protocol Module: ObservableObject, Hashable {
   associatedtype ObjectType
+  var id: UUID { get set }
   //Should be @Published in implementation, no way to do this yet in a protocol
   var object: ObjectType? { get set }
   var objectPublisher: Published<ObjectType?>.Publisher { get }
 }
 
+public extension Module {
+  static func ==(lhs: Self, rhs: Self) -> Bool {
+    return lhs.id == rhs.id
+  }
+}
