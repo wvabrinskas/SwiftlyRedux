9dfc70a4bd4863305ef79d219e03bc458eb4ab83
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-01-06 09:38:53 -0500

    refactored

--- Tests/SwiftlyReduxTests/SwiftlyReduxTests.swift
+++ Tests/SwiftlyReduxTests/SwiftlyReduxTests.swift
@@ -2,67 +2,6 @@ import XCTest
 import Combine
 @testable import SwiftlyRedux
 
-struct TestData {
-  static let initialWord = "init"
-  static let wordAdding = "test"
-  static var initialData: [String] {
-    return [Self.initialWord]
-  }
-  static var expectedData: [String] {
-    return [Self.initialWord, Self.wordAdding]
-  }
-}
-
-final class SwiftlyModule: Module {
-
-  enum SubjectIdentifiers: Int, SubjectIdentifier {
-
-    case data
-    
-    var stringValue: String {
-      return "\(self.rawValue)"
-    }
-    
-  }
-  
-  var subjects: [String : AnySubject] = [:]
-  
-  init() {
-    self.addSubject(TestData.initialData, identifier: SubjectIdentifiers.data)
-  }
-  
-  func addData() {
-    var newData: [String]? = self.getSubject(id: SubjectIdentifiers.data)?.object
-    newData?.append(TestData.wordAdding)
-    self.updateSubject(value: newData, identifier: SubjectIdentifiers.data)
-  }
-}
-
-struct SwiftlySubscription: StateSubscription, SwiftlySubscriptionDescription {
-  
-  typealias TModule = SwiftlyModule
-  var module: SwiftlyModule = SwiftlyModule()
-  
-  func addData() {
-    self.module.addData()
-  }
-}
-
-protocol SwiftlySubscriptionDescription {
-  func addData()
-}
-
-final class SwiftlyStateHolder: StateHolder, SwiftlySubscriptionDescription {
-  var subscriptions: [String : AnySubscription] = [:]
-  
-  init() {
-    self.addSubscription(sub: SwiftlySubscription())
-  }
-    
-  func addData() {
-    self.getSubscription(type: SwiftlySubscription.self)?.addData()
-  }
-}
 
 final class SwiftlyReduxTests: XCTestCase {
   let state = SwiftlyStateHolder()
